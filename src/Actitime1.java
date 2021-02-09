520e2666cf909b1a940c11dea907cf8cc9eb8057
Sushant6862 <76429729+Sushant6862@users.noreply.github.com>
2021-02-09 16:04:53 +0530

    Update Actitime1.java

--- src/Actitime1.java
+++ src/Actitime1.java
@@ -22,7 +22,7 @@ public class Actitime1
 			
 		}
 		System.out.println(driver.getTitle());
-		hii...!! i am sushant on github
+		//hii...!! i am sushant, i have create acoount on github and also changes in this file.
 	}
 
 }
